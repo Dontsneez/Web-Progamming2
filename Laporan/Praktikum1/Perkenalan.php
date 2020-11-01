6b6986c8b9e9fa24d38cb5a252e29f974b0d5720
Ighfar N S <dontsneezing23@gmail.com>
2020-11-01 23:38:17 +0700

    Laporan Praktikum

---
+++ Laporan/Praktikum1/Perkenalan.php
@@ -0,0 +1,11 @@
+<?php
+class Perkenalan extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "<h1>Nama Saya Ighfar Nazar S
+            <h1>Saya tinggal di daerah Randugunting
+            <h1>Olah raga yang saya sukai adalah catur";
+    }
+}
---
+++ PustakaBookingWP/application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Ighfar</li>
+        <li>Nama Belakang : Nazar S</li>
+    </ul>
+
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Randugunting 161, Tegal</li> </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+    <li>Tegal</li></ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Catur</li>
+        <li>Bersepeda</li>
+    </ul>
+</section>
